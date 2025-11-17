ba0235804d172a59713cb77cd3ae835e4239b719
divyansh2007 <divyanshg6289@gmail.com>
2025-11-17 11:27:59 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+# --------------------------
+# Load your trained model
+# --------------------------
+model = pickle.load(open("./notebook/model.pkl", "rb"))
+vectorizer = pickle.load(open("notebook/vectorise.pkl", "rb"))
+# Make sure you saved both during training
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    review = request.form.get("review")
+
+    # Transform the review using same TF-IDF
+    transformed = vectorizer.transform([review])
+
+    # Predict
+    pred = "positive" if model.predict(transformed) == 1 else "negative"
+
+    # For probability (optional)
+    # prob = model.predict_proba(transformed)[0]
+
+    # Render result back on page
+    return render_template("index.html", sentiment=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
